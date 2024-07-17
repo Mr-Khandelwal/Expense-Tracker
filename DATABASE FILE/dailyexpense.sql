1c34d326a29a91fe38e01cc2a22c6c9843d4c110
Rajat khandelwal <7878rajatkhandelwal@gmail.com>
2024-07-17 11:54:04 +0530

    database updation

--- DATABASE FILE/dailyexpense.sql
+++ DATABASE FILE/dailyexpense.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 23, 2021 at 07:16 PM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Generation Time: Jul 17, 2024 at 08:22 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -31,8 +32,45 @@ CREATE TABLE `expenses` (
   `user_id` varchar(15) NOT NULL,
   `expense` int(20) NOT NULL,
   `expensedate` varchar(15) NOT NULL,
-  `expensecategory` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `expensecategory` varchar(50) NOT NULL,
+  `project_id` int(11) DEFAULT NULL,
+  `expenditurefor` varchar(250) DEFAULT NULL,
+  `BillType` varchar(100) DEFAULT NULL,
+  `PaymentBy` varchar(100) DEFAULT NULL,
+  `PaymentType` varchar(50) DEFAULT NULL,
+  `remarks` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `expenses`
+--
+
+INSERT INTO `expenses` (`expense_id`, `user_id`, `expense`, `expensedate`, `expensecategory`, `project_id`, `expenditurefor`, `BillType`, `PaymentBy`, `PaymentType`, `remarks`) VALUES
+(87, '7', 255, '2024-06-26', 'Accomodation', 6, 'hotel ruby', 'UnBilled', 'Karan', 'Card', ''),
+(88, '7', 100, '2024-07-10', 'Transport', 1, 'ola', 'UnBilled', 'Rajat', 'Cash', ''),
+(107, '7', 5, '2024-07-16', 'Food', 1, 'milk oats', 'Billed', 'Rakesh', 'Cash', ''),
+(108, '7', 100, '2024-07-16', 'Food', 9, 'butter milk bread', 'UnBilled', 'Rajat', 'Card', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projects`
+--
+
+CREATE TABLE `projects` (
+  `project_id` int(11) NOT NULL,
+  `project_name` varchar(255) NOT NULL,
+  `investment_limit` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `projects`
+--
+
+INSERT INTO `projects` (`project_id`, `project_name`, `investment_limit`) VALUES
+(1, 'Expense Trackker', 20000.00),
+(6, 'xyz', 10000.00),
+(9, 'Construction Noida', 15820.00);
 
 -- --------------------------------------------------------
 
@@ -48,7 +86,14 @@ CREATE TABLE `users` (
   `profile_path` varchar(50) NOT NULL DEFAULT 'default_profile.png',
   `password` varchar(50) NOT NULL,
   `trn_date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `firstname`, `lastname`, `email`, `profile_path`, `password`, `trn_date`) VALUES
+(7, 'Rajat ', 'khandelwal', '7878rajatkhandelwal@gmail.com', 'Untitled design (2).png', '25d55ad283aa400af464c76d713c07ad', '2024-05-30 10:53:25');
 
 --
 -- Indexes for dumped tables
@@ -58,7 +103,14 @@ CREATE TABLE `users` (
 -- Indexes for table `expenses`
 --
 ALTER TABLE `expenses`
-  ADD PRIMARY KEY (`expense_id`);
+  ADD PRIMARY KEY (`expense_id`),
+  ADD KEY `project_id` (`project_id`);
+
+--
+-- Indexes for table `projects`
+--
+ALTER TABLE `projects`
+  ADD PRIMARY KEY (`project_id`);
 
 --
 -- Indexes for table `users`
@@ -74,12 +126,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `expenses`
 --
 ALTER TABLE `expenses`
-  MODIFY `expense_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+  MODIFY `expense_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=109;
+
+--
+-- AUTO_INCREMENT for table `projects`
+--
+ALTER TABLE `projects`
+  MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `expenses`
+--
+ALTER TABLE `expenses`
+  ADD CONSTRAINT `expenses_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
